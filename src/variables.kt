f73a4d5178ae5fa2b1e44d1e2b813135b842025a
Martin Cornel <mrtncornel@gmail.com>
2019-10-13 13:26:17 +0300

    Initializing variables and declaring constants.

---
+++ src/variables.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>) {  // the entry point o a kotlin project is the main function which may take arguments as an array of string values.
+    var x:Int = 10              // initializing a variable x of type integer with initial value 10
+    var y:String= "Welcome"     // initializing a variable y of type string with this value
+
+    println(x)                  // how to print a line
+    println(y)
+
+    val z:Int = 20              // val is how to declare a constant. Constants cannot be changed
+}
